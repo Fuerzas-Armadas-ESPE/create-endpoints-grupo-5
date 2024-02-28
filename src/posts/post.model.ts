072a8acbbc45bbc35f9a7888ca18877cd54c3973
RicardoRivadeneira <87491407+RicardoRivadeneira@users.noreply.github.com>
2024-02-27 21:40:37 -0500

    Update post.model.ts

--- src/posts/post.model.ts
+++ src/posts/post.model.ts
@@ -1,16 +1,7 @@
-// post.model.ts
-import { Schema, Document } from 'mongoose';
+import { Schema } from 'mongoose';
 
-export interface Post extends Document {
-  title: string;
-  content: string;
-}
-
-export const PostSchema = new Schema<Post>(
-  {
-    _id: { type: Schema.Types.ObjectId, auto: true }, // Definir explícitamente _id como ObjectId
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-  },
-  { timestamps: true },
-); // Añadir timestamps para createdAt y updatedAt
+export const PostSchema = new Schema({
+  id_post: { type: String, required: true, unique: true },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+});
